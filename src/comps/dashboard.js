7f57cf72a981801516179854911894e7aee8f719
yfsoftcom <yfsoftcom@126.com>
2017-02-09 13:46:57 +0800

    add rechart

--- src/comps/dashboard.js
+++ src/comps/dashboard.js
@@ -1,7 +1,17 @@
 import React,{ Component } from 'react'
-
+import { Panel } from './common'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 class Dashboard extends Component {
     render ()  {
+      const data = [
+        {name: '00 : 00', uv: 4000, pv: 2400, amt: 2400},
+        {name: '01 : 00', uv: 3000, pv: 1398, amt: 2210},
+        {name: '02 : 00', uv: 2000, pv: 99, amt: 2290},
+        {name: '03 : 00', uv: 2780, pv: 3908, amt: 2000},
+        {name: '04 : 00', uv: 1890, pv: 4800, amt: 2181},
+        {name: '05 : 00', uv: 2390, pv: 3800, amt: 2500},
+        {name: '06 : 00', uv: 3490, pv: 4300, amt: 2100},
+      ]
       return (
         <div id="container" className="center-block">
 
@@ -85,16 +95,20 @@ class Dashboard extends Component {
               </div>
 
               <div className="col-md-6">
-                <div className="panel panel-default">
-                  <div className="panel-heading">Mem
-                    <button className="btn btn-sm pull-right">
-                      <i className="fa fa-refresh"></i>
-                    </button>
+                <Panel title="Mem">
+                  <div className="">
+                    <LineChart width={650} height={300} data={data}
+                          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                     <XAxis dataKey="name"/>
+                     <YAxis/>
+                     <CartesianGrid strokeDasharray="3 3"/>
+                     <Tooltip/>
+                     <Legend />
+                     <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+                     <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
                   </div>
-                  <div className="panel-body">
-                    <div className="bg-danger empty-chart"></div>
-                  </div>
-                </div>
+                </Panel>
               </div>
             </div>
           </div>
