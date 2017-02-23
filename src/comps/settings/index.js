b58c0aeb15b695b2956f77804c291b967f09cca7
yfsoftcom <yfsoftcom@126.com>
2017-02-23 14:07:33 +0800

    add link

---
+++ src/comps/settings/index.js
@@ -0,0 +1,4 @@
+import React,{ Component } from 'react'
+import Smtp from './smtp'
+
+export { Smtp }
--- src/comps/tools/index.js
+++ src/comps/tools/index.js
@@ -1,14 +1,4 @@
 import React,{ Component } from 'react'
 import ApiTester from './api-tester'
 
-class Tools extends Component{
-    render (){
-      return (
-        <div>
-          {this.props.children}
-        </div>
-      )
-    }
-}
-
-export { ApiTester ,Tools }
+export { ApiTester }
