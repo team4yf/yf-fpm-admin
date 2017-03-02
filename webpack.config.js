108a6a8420423faebfa1daf227fab91162f8ca6b
yfsoftcom <yfsoftcom@126.com>
2017-03-02 22:07:43 +0800

    todo

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: { 
+    app: './src/index.js', 
+    react: [
+      'react', 
+      'react-dom', 
+    ],
+    chart: [
+      'recharts',
+    ],
+    vender: [
+      'async', 'lodash', 'node-fetch',
+    ],
+    other: [
+      'pubsub-js',
+    ]
+  } ,
 
   output: {
-    path: __dirname + '/assets/',
-    filename: 'bundle.js',
-    publicPath: "/assets/"
+    path: 'assets',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
   },
 
   module: {
@@ -12,5 +30,12 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
       { test: /\.json$/, loader: "json-loader"}
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+        names: ['react', 'chart', 'vender', 'other', 'manifest'],
+    }),
+  ],
 }
+
+
